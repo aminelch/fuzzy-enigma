b11fda6b3440f4076487225c66987acb0c609ef6
Amine L'ch <aminelch95@gmail.com>
2021-01-12 00:42:18 +0100

    array manipulation

--- array-manipulation.js
+++ array-manipulation.js
@@ -10,29 +10,30 @@ let firstEpisode = new Episode('Dark Beginnings', 45, true);
 let secondEpisode = new Episode('The Mystery Continues', 45, false);
 let thirdEpisode = new Episode('An Unexpected Climax', 60, false);
 
-// Create your array here
+// Modify the array here
 // ====================================
+let episodes = [firstEpisode, secondEpisode, thirdEpisode]
 
-let episodes = [
-  [ 'Dark Beginnings', 45, true  ],
-   [ 'Dark Beginnings', 45, true  ],
-   [ 'Dark Beginnings', 45, true  ],
-   [ 'Dark Beginnings', 45, true  ]
-]
+
+episodes.unshift(new Episode('Vanilla Sky',108, true)) // ajouter au debut 
+episodes.pop() // supprime le dernier element du tab 
+episodes.push(new Episode('x-Men',77, false)) // ajouter à la fin du tab 
 
 // ====================================
 
 const body = document.querySelector('body');
-
-for(let i = 0; i < 2; i++) {
+let numberOfEpisodes = episodes.length
+document.querySelector('#episodes').innerText = numberOfEpisodes;
+console.log(episodes)
+for(let episode of episodes) {
   let newDiv = document.createElement('div');
   newDiv.classList.add('series-frame');
   let newTitle = document.createElement('h2');
   newTitle.innerText = 'The Story of Tau';
   let newParagraph = document.createElement('p');
-  newParagraph.innerText = `${episodes[i].title}
-${episodes[i].duration} minutes
-${episodes[i].hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+  newParagraph.innerText = `${episode.title}
+${episode.duration} minutes
+${episode.hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
   newDiv.append(newTitle);
   newDiv.append(newParagraph);
   body.append(newDiv);
